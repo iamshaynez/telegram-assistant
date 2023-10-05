ca99ec732477f012de77e00b9b04b421b48860dd
Xiaowen.Z <xiaowen.z@outlook.com>
2023-10-05 21:55:10 +0800

    init dev

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import { initEnv } from "./env.js";
-import { errorToString } from "./utils.js";
+import { initEnv, ASSISTANT_KV } from "./env.js";
+import { renderHTML, errorToString, assistantMessage } from "./utils.js";
+import { sendMessage } from "./telegram.js";
 import { handleRequest } from "./router.js";
 
 export default {
     async fetch(request, env) {
+		let requestClone = null;
         try {
             initEnv(env);
-            //const resp = new Response("OK", {status:200})
-            const resp = await handleRequest(request, env);
-            return resp || new Response("NOTFOUND", { status: 404 });
+			requestClone = request.clone()
+            const body = await request.json();
+            const text = body.message.text;
+			const current_app = await ASSISTANT_KV.get("MODE");
+
+			if (text.startsWith(`/`)) {
+				const app = parseApp(text);
+				await ASSISTANT_KV.put("MODE", app);
+				await sendMessage(assistantMessage(`切换当前助手程序为{${app}}`))
+			}
+
+			if (current_app === null) {
+				// no app selected
+				await sendMessage(assistantMessage("请先选择一个助手程序"))
+			}
+
+			if (!text.startsWith(`/`)) {
+				const message = await handleRequest(requestClone, current_app, env);
+				await sendMessage(assistantMessage(message));
+				return new Response(message, { status: 200 });
+			}
+            
+            return new Response("COMPLETED", { status: 200 });
         } catch (e) {
             console.error(e);
-            return new Response(errorToString(e), { status: 500 });
-        }
+			await sendMessage(assistantMessage(errorToString(e)));
+            return new Response(errorToString(e), { status: 200 });
+        } finally {
+			try {
+				requestClone.json()
+			} catch (e) {
+				return new Response("Finally Handled", { status: 200 });
+			}
+		}
     },
 };
+
+function parseApp(str) {
+    const index = str.indexOf(" ");
+    if (index !== -1) {
+        return str.substring(1, index);
+    }
+    return str.substring(1);
+}
--- src/router.js
+++ src/router.js
@@ -1,43 +1,16 @@
 import { ASSISTANT_KV } from "./env.js";
-import { renderHTML, errorToString, assistantMessage } from "./utils.js";
-import { sendMessage } from "./telegram.js";
+import { renderHTML } from "./utils.js";
 
 // 入口方法，所有的流量由这个方法处置和调用
-export async function handleRequest(request, env) {
-    const { pathname } = new URL(request.url);
-    const body = await request.json();
-    const text = body.message.text;
+export async function handleRequest(request, current_app, env) {
+    console.log(`forward to app-${current_app}`);
+    //console.log(env.TRANSLATOR)
+    const res = await env[current_app.toUpperCase()].fetch(request);
+    console.log(`Debug message 2`);
+    const message = JSON.parse(await res.text()).message;
+    console.log(`Translated message: ${message}`);
 
-    console.log(`Get pathname: ${pathname}`);
-    console.log(`Get body text: ${text}`);
-    const current_app = await ASSISTANT_KV.get("MODE");
-    console.log(`Current mode: ${current_app}`);
-    if (current_app === null) {
-        // no app selected
-        await sendMessage(assistantMessage("请先选择一个助手程序"))
-        return defaultIndexAction();
-    }
-
-    if (pathname === `/` && text.startsWith(`/`)) {
-        const app = parseApp(text);
-        await ASSISTANT_KV.put("MODE", app);
-        await sendMessage(assistantMessage(`切换当前助手程序为{${app}}`))
-        return defaultIndexAction();
-    }
-
-    if (pathname === `/` && !text.startsWith(`/`)) {
-        console.log(`forward ${text} to app-${current_app}`);
-    }
-
-    return defaultIndexAction();
-}
-
-function parseApp(str) {
-    const index = str.indexOf(" ");
-    if (index !== -1) {
-        return str.substring(1, index);
-    }
-    return str.substring(1);
+    return message;
 }
 
 async function defaultIndexAction() {
